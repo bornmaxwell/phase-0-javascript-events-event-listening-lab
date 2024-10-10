c80f4261d8c1d7e79edf93cf97e1afd8cc31fede
Maxwell Born <borncmaxwell@gmail.com>
2024-10-10 16:56:26 -0500

    listening lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    // Select the element you want to add the event listener to
+    const button = document.getElementById('button');
+
+    // Add an event listener to the button that listens for 'click' events
+    button.addEventListener('click', function() {
+        alert('Button was clicked!');
+    });
 }
